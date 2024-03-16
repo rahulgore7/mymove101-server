3962bc13c8050514135232c9f45e5654926e5bd9
Rahul Gore <rahulgore575@gmail.com>
2024-03-17 00:33:46 +0530

    removed getUnifiedParser in MongoDb

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ require("dotenv").config({ path: "./config/config.env" });
 const mongoose = require('mongoose');
 const dbUrl = process.env.DB_URI;
 
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(dbUrl, {});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
