fe1470279317648a53f2ac5f2f2d8fe9b2e9baed
Rahul Gore <rahulgore575@gmail.com>
2024-04-03 20:46:03 +0530

    made a few chanegs

--- Routes/CustomerRoutes.js
+++ Routes/CustomerRoutes.js
@@ -11,7 +11,7 @@ router.post('/submit-form', async (req, res) => {
         const mongoResponse = await CustomerController.saveFormDataToMongoDB(FullData);
         console.log(mongoResponse);
         const { referralCode, ...userData } = FullData;
-        console.log(`Data we are sending to the server: ${userData}`);
+        console.log(`Data we are sending to the server: ${JSON.stringify(userData)}`);
         const response = await CustomerController.sendFormDataToServer(userData);
         res.json(response);
     } catch (error) {
