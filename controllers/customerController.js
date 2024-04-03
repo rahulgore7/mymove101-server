ec726e338ac2d92bab829ac3c509ac7e4f80b445
Rahul Gore <rahulgore575@gmail.com>
2024-04-03 20:27:17 +0530

    made all changes as it is

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -15,15 +15,14 @@ async function saveFormDataToMongoDB(formData) {
 
 async function sendFormDataToServer(formData) {
     try {
-        // const response = await axios.post(`https://mu2-staging.myutilities.com/api/referral/orders?token=${process.env.API_TOKEN}`, formData, {
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'X-Version': '1.0'
-        //     }//rkjn
-        // });
-        //console.log('Response from server:', response.data);
-        //return response.data;
-        console.log(formData);
+        const response = await axios.post(`https://mu2-staging.myutilities.com/api/referral/orders?token=${process.env.API_TOKEN}`, formData, {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Version': '1.0'
+            }//rkjn
+        });
+        console.log('Response from server:', response.data);
+        return response.data;
     } catch (error) {
         console.error('Error sending form data to server:', error.message);
         throw new Error(error.message);
